28e7386aab1ce52bea8d1d10df63c6d1bb245ffd
Michael Johnson <michaeljcantley@gmail.com>
2024-04-30 13:19:55 -0400

    Added variable for img

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import img1 from './assets/image-hero-mobile.png'
 import './App.css'
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <>
       <picture>
         <source srcSet="src/assets/image-hero-desktop.png" media="(min-width: 600px)" type="image/png" />
-        <img src="src/assets/image-hero-mobile.png" alt="" />
+        <img src={img1} alt="" />
       </picture>
     </>
     
